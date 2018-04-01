4e327f9d3985fa9484c34fd05b3abc5a6def8542
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-04-01 19:16:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Add your doToElementsInArray() function here:
+var evens = [1,2,3,4,5,6,7]
 
+evens.forEach(n => {
+  console.log(`${n} is great`)
+});
 
 // Add your changeCompletely() function here:
